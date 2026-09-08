606db5880b30c2d5ccf4f47735fd42d3ff5f3e0c
Glonk <jaspreetbath98@gmail.com>
2026-09-08 14:19:45 -0700

    Initial commit

---
+++ Labs/Dotnet101/DotnetWeb/Pages/ProcOtherDetails.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace DotnetWeb.Pages;
+
+public class ProcOtherDetailsModel : PageModel
+{
+    public void OnGet(int id)
+    {
+        Proc = Process.GetProcessById(id);
+        
+    }
+}
+
---
+++ Labs/Dotnet101/DotnetWeb/Pages/ProcsOther.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace DotnetWeb.Pages;
+
+public class ProcsOtherModel : PageModel
+{
+    public void OnGet()
+    {
+        Procs = Process.GetProcesses();
+    }
+}
+
